7f9cd4e94cf1c005841f2d6aaf7a7659592e565c
Paul Krisadaolarn <pauly.k@gmail.com>
2023-12-08 16:29:13 +0700

    feat: add Card

---
+++ src/components/Card.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+</script>
+
+<div class="card">
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: start;
+        padding: 1rem;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+
+    :global(.card > *) {
+        margin-bottom: 1rem;;
+    }
+    :global(.card > *:last-of-type) {
+        margin-bottom: 0rem;        
+    }
+    
+</style>
--- src/components/TodoInput.svelte
+++ src/components/TodoInput.svelte
@@ -1,1 +1,3 @@
 <div>Todo input</div>
+
+
